0b1bcf62dc8cb67a5915643ea27d12faf45a6c58
Anton Fomichev <av.fomichev01@yandex.ru>
2025-01-02 00:14:34 +0300

    build: add eslint prettier plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,11 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     ...eslintPluginAstro.configs.all,
+    eslintPluginPrettierRecommended,
 );
