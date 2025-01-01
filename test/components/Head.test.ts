3a7b1626208cea58a03ac19c9766f76f11ea02e2
Anton Fomichev <av.fomichev01@yandex.ru>
2025-01-02 01:07:30 +0300

    Head component (#12)

---
+++ test/components/Head.test.ts
@@ -0,0 +1,66 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import Head from "../../src/components/Head.astro";
+import author from "../../src/data/author.json";
+
+it(`renders "UTF-8" charset`, async () => {
+    const container = await AstroContainer.create();
+
+    const result = await container.renderToString(Head, {});
+
+    expect(result).toContain('<meta charset="UTF-8">');
+});
+
+it(`renders "viewport" meta tag`, async () => {
+    const container = await AstroContainer.create();
+
+    const result = await container.renderToString(Head, {});
+
+    expect(result).toContain(
+        '<meta content="width=device-width, initial-scale=1.0" name="viewport">',
+    );
+});
+
+it(`renders description, keywords meta tags`, async () => {
+    const container = await AstroContainer.create();
+    const props = {
+        description: "Test Description",
+        keywords: "astro, vitest, container",
+    };
+
+    const result = await container.renderToString(Head, { props });
+
+    expect(result).toContain(
+        '<meta content="Test Description" name="description">',
+    );
+    expect(result).toContain(
+        '<meta content="astro, vitest, container" name="keywords">',
+    );
+});
+
+it(`renders title tag`, async () => {
+    const container = await AstroContainer.create();
+    const props = { title: "Test Title" };
+
+    const result = await container.renderToString(Head, { props });
+
+    expect(result).toContain("<title>Test Title</title>");
+});
+
+it(`renders with empty props`, async () => {
+    const container = await AstroContainer.create();
+
+    const result = await container.renderToString(Head, {});
+
+    expect(result).toContain("<title></title>");
+    expect(result).toContain('<meta name="description">');
+    expect(result).toContain('<meta name="keywords">');
+});
+
+it("renders author meta tag", async () => {
+    const container = await AstroContainer.create();
+
+    const result = await container.renderToString(Head, {});
+
+    const authorContent = `<meta content="${author.name}, ${author.role}. Contact: ${author.email}, ${author.website}" name="author">`;
+    expect(result).toContain(authorContent);
+});
